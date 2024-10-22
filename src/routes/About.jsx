6aec6e06ab774fa22786971d7170e42803f5c2b7
FrosT2k5 <yashpatil2k5@gmail.com>
2024-10-22 19:28:47 +0530

    routes: Update about page
    Signed-off-by: FrosT2k5 <yashpatil2k5@gmail.com>

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import classes from '../routes/css/dashboard.module.css';
 
 const About = () => {
     return (
-        <div>
-            <h1>About Us</h1>
-            <p>This is the about page of our application.</p>
-            <p>Here you can find information about what we do.</p>
-        </div>
+        <>
+            <div className={classes.headingBox}>
+                <h4 className={classes.textBold}>About Us</h4>
+                <h4>Here you can find information about who we are and what we do.</h4>
+            </div>
+
+            <div className={`${classes.smallBox} ${classes.selfCenter}`}>
+                <img src="/yash.svg" width="30%" />
+                <h4 className={classes.textBold}>Yash Patil</h4>
+                <h4>I&apos;m passionate about Programming, Sci-fi, OpenSource and Philosophy while also Reading books.</h4>
+            </div>
+
+            <div className={`${classes.smallBox} ${classes.selfCenter}`}>
+                <img src="/sairaj.svg" width="30%" />
+                <h4 className={classes.textBold}>Sairaj Pai</h4>
+                <h4>I&apos;m passionate about tabla, sci-fi, sitcoms, and movies, while also exploring coding and embracing diverse experiences.</h4>
+            </div>
+
+            <div className={`${classes.smallBox} ${classes.selfCenter}`}>
+                <img src="/nandini.svg" width="30%" />
+                <h4 className={classes.textBold}>Nandini Nichite</h4>
+                <h4>Passionate about reading books and diving into coding, exploring new realms in literature and technology with enthusiasm.</h4>
+            </div>
+        </>
     );
 };
 
