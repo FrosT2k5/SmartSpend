d0c9c9f5194671cc7afc593ab0cfcf83139ead05
FrosT2k5 <yashpatil2k5@gmail.com>
2024-11-19 22:55:36 +0530

    routes: Error.jsx: Console log the error makes it easier to debug
    Signed-off-by: FrosT2k5 <yashpatil2k5@gmail.com>

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -3,6 +3,7 @@ import { useRouteError } from "react-router-dom";
 function Error() {
   const error = useRouteError();
 
+  console.log(error);
   return (
     <>
       <div>Oops! An error...</div>
